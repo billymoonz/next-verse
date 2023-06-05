194bfb42161de5151b3a267b5f80e33cc84d1af8
Moonz <55450457+billymoonz@users.noreply.github.com>
2023-06-05 21:49:44 +0100

    test

---
+++ app/(marketing)/news-letter/page.js
@@ -0,0 +1,7 @@
+export const metadata = {
+    title: 'Subscribe to our News Letter'
+}
+
+export default async function NewsLetter() {
+    return ('News Letter')
+}
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,3 @@
-'use server'
-
 import '@/styles/globals.css'
 import '@/styles/main.scss';
 
